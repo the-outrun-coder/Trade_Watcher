2a2affa17e113e3b75d4edc75ff5d9d0fd38aa04
David Pineda <david.pineda.codes@gmail.com>
2023-11-19 09:50:23 -0500

    Add and test React Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Badge, Button } from 'react-bootstrap'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +30,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <h1>
+        <Badge bg="secondary">New</Badge>
+        <Button variant="primary">Exampe... tuh-duh</Button>
+      </h1>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
