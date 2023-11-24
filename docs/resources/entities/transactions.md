4e426bed2072b4f0ec27d77b1a292d2c602f32d4
David Pineda <david.pineda.codes@gmail.com>
2023-11-24 15:14:20 -0500

    Scaffold add transaction form

--- docs/resources/entities/transactions.md
+++ docs/resources/entities/transactions.md
@@ -2,9 +2,19 @@
 
 - date
 - time
-- amount
-- price
+- amount-acquired
+- amount-purchased
 - subtotal
 - fees
+- actual
+
 - cost
-- 
+
+
+## Via Binance "Buy Order" trx
+- type
+- Filled / Amount
+- Avg. Price
+- Date
+- Fee
+- Total (Cost)
--- src/features/add-record/Add-Transaction.tsx
+++ src/features/add-record/Add-Transaction.tsx
@@ -1,8 +1,38 @@
-
+import { Button, Form, InputGroup } from "react-bootstrap";
 
 function AddTransaction() {
   return (
-    <h2> Add transaction form goes here. </h2>
+    <Form>
+      <Form.Group controlId="formAddTrx_acquiredAmount">
+        <InputGroup className="mb-3">
+          <InputGroup.Text>Amount Purchased</InputGroup.Text>
+          <Form.Control/>
+        </InputGroup>
+      </Form.Group>
+
+      <Form.Group controlId="formAddTrx_acquiredSymbol">
+        <InputGroup className="mb-3">
+          <InputGroup.Text>Purchase Symbol</InputGroup.Text>
+          <Form.Control/>
+        </InputGroup>
+      </Form.Group>
+
+      <Form.Group controlId="formAddTrx_purchaseAmount">
+        <InputGroup className="mb-3">
+          <InputGroup.Text>Amount Paid</InputGroup.Text>
+          <Form.Control/>
+        </InputGroup>
+      </Form.Group>
+
+      <Form.Group controlId="formAddTrx_purchaseSymbol">
+        <InputGroup className="mb-3">
+          <InputGroup.Text>Paid Symbol</InputGroup.Text>
+          <Form.Control/>
+        </InputGroup>
+      </Form.Group>
+
+      <Button type="submit" variant="primary">Add Transaction</Button>
+    </Form>
   );
 }
 
