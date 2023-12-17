c3a2f4d88b76eb1c2b451b75e0d9f302848a45f2
David Pineda <david.pineda.codes@gmail.com>
2023-12-17 08:45:04 -0500

    setup DynamicContainer to receive children for centered column controlled layout

--- UI_REACT/src/App.tsx
+++ UI_REACT/src/App.tsx
@@ -6,6 +6,7 @@ import { store } from './store';
 import AddRecord from './features/add-record';
 import TransactionList from './features/transaction-list';
 import { fetchUsers } from './store/slice/users';
+import DynamicContainer from './component/layout/dynamic-container';
 
 
 function App() {
@@ -15,7 +16,6 @@ function App() {
 
 	return (
 		<Provider store={store}>
-			
 
 			<Navbar expand="lg">
 				<Container>
@@ -41,41 +41,42 @@ function App() {
 				</Container>
 			</Navbar>
 
-			<div className="proxy-router-feature-accordion">
-				<Accordion defaultActiveKey="2">
-					<Accordion.Item eventKey='0'>
-						<Accordion.Header>
-							Login
-						</Accordion.Header>
-						<Accordion.Body>
-							{/* TODO - LOGIN ROUTE AND COMPONENT */}
-							<h1>Login Feature goes here</h1>
-						</Accordion.Body>
-					</Accordion.Item>
+			<DynamicContainer>
+				<div className="proxy-router-feature-accordion">
+					<Accordion defaultActiveKey="2">
+						<Accordion.Item eventKey='0'>
+							<Accordion.Header>
+								Login
+							</Accordion.Header>
+							<Accordion.Body>
+								{/* TODO - LOGIN ROUTE AND COMPONENT */}
+								<h1>Login Feature goes here</h1>
+							</Accordion.Body>
+						</Accordion.Item>
 
-					<Accordion.Item eventKey='1'>
-						<Accordion.Header>Dashboard</Accordion.Header>
-						<Accordion.Body>
-							<h1>Dashboard goes here</h1>
-						</Accordion.Body>
-					</Accordion.Item>
+						<Accordion.Item eventKey='1'>
+							<Accordion.Header>Dashboard</Accordion.Header>
+							<Accordion.Body>
+								<h1>Dashboard goes here</h1>
+							</Accordion.Body>
+						</Accordion.Item>
 
-					<Accordion.Item eventKey='2'>
-						<Accordion.Header>Add Record</Accordion.Header>
-						<Accordion.Body>
-							<AddRecord/>
-						</Accordion.Body>
-					</Accordion.Item>
+						<Accordion.Item eventKey='2'>
+							<Accordion.Header>Add Record</Accordion.Header>
+							<Accordion.Body>
+								<AddRecord/>
+							</Accordion.Body>
+						</Accordion.Item>
 
-					<Accordion.Item eventKey='3'>
-						<Accordion.Header>Transaction List</Accordion.Header>
-						<Accordion.Body>
-							<TransactionList/>
-						</Accordion.Body>
-					</Accordion.Item>
-				</Accordion>
-			</div>  
-
+						<Accordion.Item eventKey='3'>
+							<Accordion.Header>Transaction List</Accordion.Header>
+							<Accordion.Body>
+								<TransactionList/>
+							</Accordion.Body>
+						</Accordion.Item>
+					</Accordion>
+				</div>
+			</DynamicContainer>
 		</Provider>
 	)
 }
